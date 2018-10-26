7e8d123364d5e96272dbe99037628f68072e33c6
pierthodo <pierthodo@gmail.com>
2018-10-26 16:30:43 -0400

    update PPO2

--- dev/baselines/common/cmd_util.py
+++ dev/baselines/common/cmd_util.py
@@ -130,6 +130,7 @@ def common_arg_parser():
     parser.add_argument('--reward_scale', help='Reward scale factor. Default: 1.0', default=1.0, type=float)
     parser.add_argument('--save_path', help='Path to save trained model to', default=None, type=str)
     parser.add_argument('--play', default=False, action='store_true')
+    parser.add_argument('--log-disable',default=False,action='store_true')
     return parser
 
 def robotics_arg_parser():
